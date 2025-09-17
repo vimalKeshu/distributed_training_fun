9c72f311f3fa03c5b9c98edb382492ea87e5a740
Vimal <vmal.chaudhari@gmail.com>
2025-09-17 17:14:11 +0530

    Update build.sh

--- pipeline_parallelism/build.sh
+++ pipeline_parallelism/build.sh
@@ -8,11 +8,11 @@ echo "▶ Using version: $version"
 # 2) Build the image locally without cache
 docker build --no-cache -t dtpp:"${version}" .
 
-# 3) Tag the image for GCR
+# 3) Tag the image
 docker tag dtpp:"${version}" \
   gcr.io/mde-cloud/image-repo/dtpp:"${version}"
 
-# 4) Push to GCR
-docker push gcr.io/mde-cloud/image-repo/dtpp:"${version}"
+# 4) Push to docker
+docker push ${DOCKER_URL}/dtpp:"${version}"
 
-echo "✅ Docker image gcr.io/mde-cloud/image-repo/dtpp:${version} built, tagged, and pushed to gcr.io/mde-cloud/image-repo."
+echo "✅ Docker image ${DOCKER_URL}/dtpp:${version} built, tagged, and pushed to ${DOCKER_URL}."
